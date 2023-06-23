f7985a674c62c0cd282f02f893ba7afa715a856e
zainab7681051 <zainab76810510@gmail.com>
2023-06-23 23:54:04 +0300

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace WebScraperForDisneyMovies
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Movie> movies = new();
+            string site = "https://www.imdb.com",
+            list = "/list/ls059383351/",
+            xPath = "//*[@class='lister-item mode-detail']";
+            movies = Scraper.ScrapeData(site, list, xPath);
+            // using var db = new MovieDatabaseContext();
+            foreach (var movie in movies)
+            {
+                Console.WriteLine($"adding \"{movie.title}\" to the database...");
+                //     db.Add(movie);
+                //     db.SaveChanges();
+            }
+
+
+            // Read
+            // Console.WriteLine("Querying for a Movie");
+            // var FoundMovie = db.disney_movies
+            //     .Find(19);
+            // if (FoundMovie is null)
+            // {
+            //     throw new NullReferenceException("wrong Id parameter or movie is not present in the database");
+
+            // }
+            // Console.WriteLine(FoundMovie.title + "\t" + FoundMovie.year + "\n" + FoundMovie.directors + "\n\t---------------------------");
+        }
+
+    }
+}
