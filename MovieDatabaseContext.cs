14189e747af185816ee0a3472eecacc1591815fd
zainab7681051 <zainab76810510@gmail.com>
2023-06-23 22:59:52 +0300

    database context

---
+++ MovieDatabaseContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+public class MovieDatabaseContext : DbContext
+{
+    public DbSet<Movie> disney_movies { get; set; }
+    public string DbPath { get; }
+
+    public MovieDatabaseContext()
+    {
+        DbPath = "./DisneyMoviesDB.db";
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    => options.UseSqlite($"Data Source={DbPath}");
+
+}
