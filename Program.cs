4018ba695e7b11b5ea6c3da3cef7a409e4fef211
zainab7681051 <zainab76810510@gmail.com>
2023-06-25 04:11:50 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,7 @@
-﻿
-using System.Diagnostics;
+﻿using System.Diagnostics;
 
 namespace WebScraperForDisneyMovies
 {
-
     public class Program
     {
         static void Main(string[] args)
@@ -14,32 +12,33 @@ namespace WebScraperForDisneyMovies
             string site = "https://www.imdb.com",
             list = "/list/ls059383351/",
             xPath = "//*[@class='lister-item mode-detail']";
-            movies = Scraper.ScrapeData(site, list, xPath);
+            var scraper = new Scraper();
+            movies = scraper.ScrapeData(site, list, xPath);
 
-            // using var db = new MovieDatabaseContext();
+            using var db = new MovieDatabaseContext();
 
             //Write            
-            // foreach (var movie in movies)
-            // {
-            //     Console.WriteLine($"adding \"{movie.title}\" to the database...");
-            //     db.Add(movie);
-            //     db.SaveChanges();
-            // }
+            foreach (var movie in movies)
+            {
+                Console.WriteLine($"adding \"{movie.title}\" to the database...");
+                db.Add(movie);
+                db.SaveChanges();
+            }
 
             // Read
-            // Console.WriteLine("\nQuerying for a Movie");
-            // var FoundMovie = db.disney_movies
-            //     .Find(19);
-            // if (FoundMovie is null)
-            // {
-            //     throw new NullReferenceException("wrong Id parameter or movie is not present in the database");
+            Console.WriteLine("\nQuerying for a Movie");
+            var FoundMovie = db.disney_movies
+                .Find(19);
+            if (FoundMovie is null)
+            {
+                throw new NullReferenceException("wrong Id parameter or movie is not present in the database");
 
-            // }
-            // Console.WriteLine(FoundMovie.title + "\t" + FoundMovie.year + "\n" + FoundMovie.directors + "\n\t---------------------------");
+            }
+            Console.WriteLine(FoundMovie.title + "\t" + FoundMovie.year + "\n" + FoundMovie.directors + "\n\t---------------------------");
             sw.Stop();
             string ExecutionTimeTaken = string.Format("{0} minutes, {1}seconds", sw.Elapsed.Minutes, sw.Elapsed.Seconds);
 
-            Console.WriteLine("program finished in: " + ExecutionTimeTaken);
+            Console.WriteLine("\nprogram finished in: " + ExecutionTimeTaken);
 
 
         }
--- Scraper.cs
+++ Scraper.cs
@@ -3,157 +3,155 @@ using HtmlAgilityPack;
 
 namespace WebScraperForDisneyMovies
 {
-    public static class Scraper
+    public class Scraper
     {
-        public static List<Movie> ScrapeData(string site, string list, string xPath)
+        public List<Movie> ScrapeData(string site, string list, string xPath)
         {
             List<Movie> movies = new();
             string url = site + list;
+            var web = new HtmlAgilityPack.HtmlWeb();
+            var doc = web.Load(url);
+            var docNodes = doc.DocumentNode.SelectNodes(xPath);
+            if (docNodes is null)
+            {
+                throw new NullReferenceException("docNodes is null");
+            }
+
+            int nodeCollectionLength = docNodes.ToArray().Length;
+            List<Movie> movieList = new();
+
+            Console.WriteLine("\nScraping the data....");
+            for (int i = 0; i < nodeCollectionLength; i++)
+            {
+                UpdateProgressBar(i, nodeCollectionLength - 1);
+                Movie movie = ScrapePage(docNodes[i], site);
+                if (movie is null) continue;
+                movies.Add(movie);
+            }
+            Console.WriteLine("\nScraping Stage completed.\n");
+            return movies;
+        }
+
+        private Movie ScrapePage(HtmlNode item, string site)
+        {
             string title = "", year = "", link = "",
                 image = "", runtime = "", genre = "",
                 summary = "", rating = "", metascore = "",
                 cast = "", directors = "", stars = "",
                 directorsPattern = @"Director(?:s)?:\s*(.*?)(?:\s*\||$)",
                 starsPattern = @"Stars:\s*(.*?)(?:\s*\||$)";
-
-
-            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
-            HtmlDocument doc = web.Load(url);
-            HtmlDocument newTab;
-            HtmlNodeCollection metaTags;
-            HtmlNodeCollection docNodes = doc.DocumentNode.SelectNodes(xPath);
-            if (docNodes is null)
+            try
             {
-                throw new NullReferenceException("docNodes is null");
-            }
-
-            int i = 0, nodeCollectionLength = docNodes.ToArray().Length;
-
-            Console.WriteLine("Scraping the data...");
-            foreach (var item in docNodes)
-            {
-                i++;
-                // Update progress bar
-                UpdateProgressBar(i, nodeCollectionLength);
-                try
+                if (item is null)
                 {
-                    if (item is null)
-                    {
-                        continue;
-                    }
-                    //LINK
-                    link = item.Element("div").NextSibling.NextSibling
-                    .Element("h3").ChildNodes["a"].Attributes["href"].Value.Trim();
+                    return null;
+                }
+                //LINK
+                link = item.Element("div").NextSibling.NextSibling
+                .Element("h3").ChildNodes["a"].Attributes["href"].Value.Trim();
 
-                    //TITLE
-                    title = item.Element("div").NextSibling.NextSibling
-                    .Element("h3").ChildNodes["a"].InnerText.Trim();
+                //TITLE
+                title = item.Element("div").NextSibling.NextSibling
+                .Element("h3").ChildNodes["a"].InnerText.Trim();
 
-                    // // //YEAR
-                    year = item.Element("div").NextSibling.NextSibling
-                    .Element("h3").Elements("span").Where(i => i.HasClass("lister-item-year"))
+                //YEAR
+                year = item.Element("div").NextSibling.NextSibling
+                .Element("h3").Elements("span").Where(i => i.HasClass("lister-item-year"))
+                .Single().InnerText.Trim();
+
+
+                // GENRE AND RUNTIME
+                var detail = item.Element("div").NextSibling.NextSibling.Element("h3")
+                .NextSibling.NextSibling.ChildNodes;
+
+                genre = detail.Where(d => d.HasClass("genre")).Single().InnerText.Trim();
+
+                runtime = detail.Where(d => d.HasClass("runtime")).Single().InnerText.Trim();
+
+                // RATING
+                rating = item.Element("div")
+                    .NextSibling.NextSibling.Element("div")
+                    .Element("div")
+                    .ChildNodes.Where(d => d.HasClass("ipl-rating-star__rating"))
                     .Single().InnerText.Trim();
 
+                // //METASCORE
+                metascore = item.Element("div")
+                    .NextSibling.NextSibling.Element("div")
+                    .NextSibling.NextSibling.Element("span").InnerText.Trim();
+                // //SUMMARY
+                summary = item.Element("div")
+                    .NextSibling.NextSibling.Element("div")
+                    .NextSibling.NextSibling.NextSibling.NextSibling.InnerText.Trim();
 
-                    // //GENRE AND RUNTIME
-                    var detail = item.Element("div").NextSibling.NextSibling.Element("h3")
-                    .NextSibling.NextSibling.ChildNodes;
+                // //CAST(DIRECTORS & STARS)
+                cast = item.Element("div")
+                    .NextSibling.NextSibling.Element("div")
+                    .NextSibling.NextSibling.NextSibling
+                    .NextSibling.NextSibling.NextSibling.InnerText.Trim();
 
-                    genre = detail.Where(d => d.HasClass("genre")).Single().InnerText.Trim();
+                // Extract Directors
+                var directorsMatch = Regex.Match(cast, directorsPattern, RegexOptions.Singleline);
+                directors = directorsMatch.Groups[1].Value.Trim();
 
-                    runtime = detail.Where(d => d.HasClass("runtime")).Single().InnerText.Trim();
+                //  Extract Stars
+                var starsMatch = Regex.Match(cast, starsPattern, RegexOptions.Singleline);
+                stars = starsMatch.Groups[1].Value.Trim();
 
-                    // //RATING
-                    rating = item.Element("div")
-                        .NextSibling.NextSibling.Element("div")
-                        .Element("div")
-                        .ChildNodes.Where(d => d.HasClass("ipl-rating-star__rating"))
-                        .Single().InnerText.Trim();
+                // //IMAGE
+                var web = new HtmlAgilityPack.HtmlWeb();
+                var newTab = web.Load(site + link);
+                var metaTags = newTab.DocumentNode.SelectNodes("/html/head/meta");
 
-                    // //METASCORE
-                    metascore = item.Element("div")
-                        .NextSibling.NextSibling.Element("div")
-                        .NextSibling.NextSibling.Element("span").InnerText.Trim();
-                    // //SUMMARY
-                    summary = item.Element("div")
-                        .NextSibling.NextSibling.Element("div")
-                        .NextSibling.NextSibling.NextSibling.NextSibling.InnerText.Trim();
-
-                    // //CAST(DIRECTORS & STARS)
-                    cast = item.Element("div")
-                        .NextSibling.NextSibling.Element("div")
-                        .NextSibling.NextSibling.NextSibling
-                        .NextSibling.NextSibling.NextSibling.InnerText.Trim();
-
-                    // Extract Directors
-                    var directorsMatch = Regex.Match(cast, directorsPattern, RegexOptions.Singleline);
-                    directors = directorsMatch.Groups[1].Value.Trim();
-
-                    //  Extract Stars
-                    var starsMatch = Regex.Match(cast, starsPattern, RegexOptions.Singleline);
-                    stars = starsMatch.Groups[1].Value.Trim();
-
-                    // //IMAGE
-                    newTab = web.Load(site + link);
-                    metaTags = newTab.DocumentNode.SelectNodes("/html/head/meta");
-
-                    foreach (var metaTag in metaTags)
-                    {
-                        string property = metaTag.GetAttributeValue("property", "not found");
-                        if (property == "og:image")
-                        {
-                            image = metaTag.GetAttributeValue("content", "not found");
-                        }
-                    }
-                }
-                catch (Exception e)
+                foreach (var metaTag in metaTags)
                 {
-                    string stackT = e.StackTrace;
-                    string eType = e.GetType().ToString();
-                    if (eType == "System.NullReferenceException")
+                    string property = metaTag.GetAttributeValue("property", "not found");
+                    if (property == "og:image")
                     {
-
-                        if (stackT.Contains("line 48"))
-                            year = "";
-                        if (stackT.Contains("line 56"))
-                            genre = "";
-                        if (stackT.Contains("line 58"))
-                            runtime = "";
-                        if (stackT.Contains("line 68"))
-                            metascore = "";
+                        image = metaTag.GetAttributeValue("content", "not found");
                     }
-                    else
-                    {
-                        Console.WriteLine(e);
-                    }
-
-                }
-                finally
-                {
-
-                    movies.Add(new Movie
-                    {
-                        title = title,
-                        year = year,
-                        link = link,
-                        image = image,
-                        runtime = runtime,
-                        genre = genre,
-                        summary = summary,
-                        rating = rating,
-                        metascore = metascore,
-                        directors = directors,
-                        stars = stars
-                    });
-
                 }
             }
-            Console.WriteLine("\nScraping Stage completed.\n");
-            return movies;
+            catch (Exception e)
+            {
+                string stackT = e.StackTrace;
+                string eType = e.GetType().ToString();
+                if (eType == "System.NullReferenceException")
+                {
+
+                    if (stackT.Contains("line 48"))
+                        year = "";
+                    if (stackT.Contains("line 56"))
+                        genre = "";
+                    if (stackT.Contains("line 58"))
+                        runtime = "";
+                    if (stackT.Contains("line 68"))
+                        metascore = "";
+                }
+                else
+                {
+                    Console.WriteLine(e);
+                }
+
+            }
+
+            return new Movie
+            {
+                title = title,
+                year = year,
+                link = link,
+                image = image,
+                runtime = runtime,
+                genre = genre,
+                summary = summary,
+                rating = rating,
+                metascore = metascore,
+                directors = directors,
+                stars = stars
+            };
         }
 
-
-        private static void UpdateProgressBar(int currentIteration, int totalIterations)
+        private void UpdateProgressBar(int currentIteration, int totalIterations)
         {
             const int progressBarWidth = 50;
 
@@ -172,7 +170,5 @@ namespace WebScraperForDisneyMovies
             Console.CursorLeft = progressBarWidth + 1;
             Console.Write("  " + percentageString);
         }
-
-
     }
 }
